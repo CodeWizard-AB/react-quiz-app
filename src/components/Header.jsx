9083496092fcb54c3f10eb8d3de3e86cd4bbbf57
Anunay Argha <anunayargha@gmail.com>
2024-04-06 09:35:10 +0600

    project on the way

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+	return (
+		<figure className="app-header">
+			<img src="react.svg" alt="react" />
+			<figcaption>
+				<h1>the react quiz</h1>
+			</figcaption>
+		</figure>
+	);
+}
+
+export default Header;
